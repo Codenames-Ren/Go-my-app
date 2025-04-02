47dafc921b36586742c303be5d471f7b41a0b220
Codenames-Ren <renmizuki38@gmail.com>
2025-04-02 20:46:35 +0700

    Menambahkan rute endpoint untuk login dan rute grup untuk admin

--- routes/userRoute.go
+++ routes/userRoute.go
@@ -11,8 +11,28 @@ func UserRoutes(router *gin.Engine) {
 	userGroup := router.Group("/users")
 	{
 		userGroup.POST("/register", controllers.Register)
+		userGroup.POST("/login", controllers.Login)
 		userGroup.GET("/profile", middlewares.AuthMiddleware(), func(c *gin.Context) {
 			c.JSON(200, gin.H{"message": "Welcome to your profile!"})
+		})
+	}
+
+	//route group admin
+	adminGroup := router.Group("/admin")
+	adminGroup.Use(middlewares.AuthMiddleware())
+	adminGroup.Use(middlewares.AdminMiddleware())
+	{
+		adminGroup.GET("/dashboard", func(c *gin.Context) {
+			username, _ := c.Get("username")
+			c.JSON(200, gin.H{
+				"message": "Welcome Admin!",
+				"admin" : username,
+			})
+		})
+
+		//nambah endpoint buat grup admin disini
+		adminGroup.GET("/users", func(c *gin.Context) {
+			c.JSON(200, gin.H{"message": "Admin access : All user data"})
 		})
 	}
 }
